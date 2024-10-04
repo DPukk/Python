dc30d9ec714ce7b4e1acc1f6acc17bcf7c87e892
DarionPukk <darion@hkhk.edu.ee>
2024-10-04 11:40:29 +0300

    Add files via upload

---
+++ Harjutus 02.py
@@ -0,0 +1,14 @@
+# Darion Pukk
+# 04. 10. 24
+# Harjutus 02
+
+#09
+arv = int(input("Lisa 10nd arv: "))
+print(bin(arv))
+print(hex(arv))
+
+# 10. Kütusekulu arvutamine
+liiter = int(input("Lisa kütusekulu: "))
+km = int(input("Lisa läbitud kilomeetrid"))
+kytusekulu = liiter/(km/100)
+print("Sinu keskmine kütusekulu on: ", kytusekulu)
